341a83bb619a38c1071ad126b81da21b2437b70f
JIJpma <d.j.ijpma@gmail.com>
2022-09-26 13:16:40 +0200

    add onclick question_add

---
+++ citizenvoice/survey_design/static/survey_design/js/add_question.js
@@ -0,0 +1,8 @@
+$(document).ready(
+    function() {
+        let button_add_survey = $('#sidebar-left-button-add-question')
+        button_add_survey.on("click", function(event) {
+            alert("Clicked add question button")
+        });
+    }
+);
